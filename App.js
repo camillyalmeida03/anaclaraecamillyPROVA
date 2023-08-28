e7a127f5e0da7c37012e7f6c353b29a0fb38fc2a
Aluno <aluno@etec.sp.gov.br>
2023-08-28 11:42:39 -0300

    anaclaraecamillyPROVA

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TelaLogin from '/src/TelaLogin';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TelaLogin/>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#CCEEFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
